af6a5d0856f6e918452c5e051992730be25cc303
Dqvidd <82197076+Dqvidd@users.noreply.github.com>
2023-05-12 12:16:11 +0000

    DownloadFilesController comentado

--- app/Http/Controllers/DownloadFilesController.php
+++ app/Http/Controllers/DownloadFilesController.php
@@ -1,5 +1,7 @@
 <?php
 
+#Este controlador de encarga de descargar los archivos al pulsar el botón verde de descarga
+
 namespace App\Http\Controllers;
 
 use Debugbar;
@@ -8,19 +10,14 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Response;
 
-
-
 class DownloadFilesController extends Controller {
    public function downloadfiles(Request $request) {
+     #En esta función, pasándole el request del nombre del archivo y su ruta, descarga usando la función response()->downlaod().
 
         $file = $request->input('file');
         $path = $request->input('path');
         $filePath = $path . '/' . $file;
         return response()->download($filePath);
 
-
-   }
-
-
-   
+   } 
 }
